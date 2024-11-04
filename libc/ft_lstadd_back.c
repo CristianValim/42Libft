36130ea010dc40fd278258e7dcaee6ad14e66006
Cristian Valim Da Silva <cvalim-d@c1r5s6.42lisboa.com>
2024-11-04 21:17:45 +0000

    lstmap

--- libc/ft_lstadd_back.c
+++ libc/ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: cvalim-d <cvalim-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:57:30 by cvalim-d          #+#    #+#             */
-/*   Updated: 2024/11/04 20:47:19 by cvalim-d         ###   ########.fr       */
+/*   Updated: 2024/11/04 21:17:10 by cvalim-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	last = ft_lstlast(*lst);
-	if (!last)
+	if (!lst)
 		*lst = new;
 	last->next = new;
+	last = ft_lstlast(*lst);
 }
