72315e4dfc1c0262a4c3d88647df92ceed9924ab
Cristian Valim Da Silva <cvalim-d@c1r5s6.42lisboa.com>
2024-11-04 20:31:56 +0000

    changing variables

--- libc/ft_strncmp.c
+++ libc/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: cvalim-d <cvalim-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 21:04:47 by cvalim-d          #+#    #+#             */
-/*   Updated: 2024/11/01 23:03:20 by cvalim-d         ###   ########.fr       */
+/*   Updated: 2024/11/04 20:31:36 by cvalim-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int	ft_strncmp(char *s1, char *s2, size_t n)
 	while (i < n && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if (i < n)
-		return (s1[i] - s2[i]);
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
